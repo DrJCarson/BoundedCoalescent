dbdacc1cc790539ddac055e71d1cfce96d7c513a
xavierdidelot <xavier.didelot@gmail.com>
2021-11-01 11:27:30 +0000

    Update test_prob.R

--- tests/testthat/test_prob.R
+++ tests/testthat/test_prob.R
@@ -3,10 +3,10 @@ context("Test simulation and likelihood functions behave as expected")
 
 test_that("Simulation and likelihood functions agree on likelihood value.", {
   set.seed(0)
-  t=c(0.4, 0.8, 1.2, 1.6, 2)
+  t=c(2000.4, 2000.8, 2001.2, 2001.6, 2002)
   l=rep(2, 5)
   ne=1.1
-  b=-0.1
+  b=2000
   expect_silent(sim<-bounded_times_sample(t=t,l=l , ne = ne, b = b, nsam = 1))
   expect_is(sim,'list')
   expect_silent(l<-bounded_times_likelihood(t=t,l=l,ne=ne,b=b,c=sim$times))
@@ -14,9 +14,9 @@ test_that("Simulation and likelihood functions agree on likelihood value.", {
   expect_equal(sim$likelihood,l)
 })
 
-test_that("Likelihood function gives expected result on small homochronous unbounded example.", {
-  t=c(2000,2000,2000)
-  l=c(1,1,1)
+test_that("Likelihood function gives expected result on small unbounded example.", {
+  t=c(2000,2001)
+  l=c(2,1)
   c=c(1999,1998)
   ne=2
   b=-Inf
